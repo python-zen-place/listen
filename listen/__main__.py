3e58d6fc9e5bd7e0dcd2a24c8edc0b403b42b743
Weilet <453546129@qq.com>
2020-02-25 12:28:33 +0800

    add command line main function

---
+++ listen/__main__.py
@@ -0,0 +1,20 @@
+import sys
+
+from listen.search import (
+	netease_music_search,
+	tencent_music_search,
+	kugou_music_search,
+	apple_music_search
+)
+
+from listen.util import formatter
+
+try:
+	keyword = sys.argv[1]
+	formatter.prettify_output('网易云音乐', netease_music_search(keyword))
+	formatter.prettify_output('QQ音乐', tencent_music_search(keyword))
+	formatter.prettify_output('酷狗音乐', kugou_music_search(keyword))
+	formatter.prettify_output('Apple Music', apple_music_search(keyword))
+except IndexError as e:
+	print('usage: listen SONG_NAME')
+	print('listen: error: the following arguments are required: SONG_NAME')
