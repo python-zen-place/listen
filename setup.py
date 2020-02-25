83c85381834ecb13702d2873991202e5c2d8daab
Weilet <453546129@qq.com>
2020-02-25 20:19:47 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='listen',   
+        version='1.0', 
+        description='An command line app to find where the song can be downloaded', 
+        author='Weilet',
+        author_email='weilet_sun@outlook.com', 
+        install_requires=[
+			'requests>=2.0.0'
+    	],
+)
