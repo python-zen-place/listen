a26e2c512f032305c007f8f7f684d3439c77a2cf
Weilet <453546129@qq.com>
2020-02-25 12:27:21 +0800

    add output formatter

---
+++ listen/util/formatter.py
@@ -0,0 +1,10 @@
+from typing import List, Tuple
+
+def prettify_output(platfrom: str, songs: List[Tuple[str, str]]) -> List[str]:
+	print(f'{platfrom}:')
+	if len(songs) == 0:
+		print('暂无版权')
+	for song in songs:
+		song_name, singer_name = song
+		print(f'{song_name}-{singer_name}')
+	print('------------------------------------------------------------------')
