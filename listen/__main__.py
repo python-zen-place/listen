589708807f1d4880e4d774626adc3bc43a04d3ce
Weilet <453546129@qq.com>
2020-02-26 21:48:09 +0800

    update. see README for more details

--- listen/__main__.py
+++ listen/__main__.py
@@ -1,5 +1,5 @@
+import getopt
 import sys
-
 from listen.search import (
 	netease_music_search,
 	tencent_music_search,
@@ -7,14 +7,43 @@ from listen.search import (
 	apple_music_search
 )
 
-from listen.utils import formatter
+from listen.utils import formatter, filter
+
+help_content = '''listen
+Usage:
+  python -m listen <song-name> [-a <artist-name>] [-w]
+  python -m listen listen -h
+Options:
+  -h --help         Show this screen.
+  -a <artist-name>  Search song of specific artist
+  -w                Exact search mode
+'''
 
 try:
-	keyword = sys.argv[1]
-	formatter.prettify_output('网易云音乐', netease_music_search(keyword))
-	formatter.prettify_output('QQ音乐', tencent_music_search(keyword))
-	formatter.prettify_output('酷狗音乐', kugou_music_search(keyword))
-	formatter.prettify_output('Apple Music', apple_music_search(keyword))
-except IndexError as e:
-	print('usage: listen SONG_NAME')
-	print('listen: error: the following arguments are required: SONG_NAME')
+	opts, args = getopt.gnu_getopt(sys.argv[1:], 'hwa:', ['help'])
+	for opt, value in opts:
+		if opt in ('-h', '--help'):
+			print(help_content)
+			sys.exit()
+
+	keyword = args[0]
+	results = {
+		'网易云音乐': netease_music_search(keyword),
+		'QQ音乐': tencent_music_search(keyword),
+		'酷狗音乐':  kugou_music_search(keyword),
+		'Apple Music': apple_music_search(keyword),
+	}
+
+	for opt, value in opts:
+		if opt in ('-w',):
+			for platform, songs in results.items():
+				results[platform] = filter.filter_blur_search(songs, keyword)
+		if opt in ('-a',):
+			for platform, songs in results.items():
+				results[platform] = filter.filter_artist(songs, value)
+	for platform, songs in results.items():
+		formatter.prettify_output(platform, songs)
+
+except getopt.GetoptError as e:
+	print(help_content)
+	print(f'listen: error: {e}')
